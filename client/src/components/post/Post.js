3777723ce0790065f74a848606ad9d5c8e8032de
sahinmehmetemre <msahin80@icloud.com>
2020-07-11 06:11:39 +0300

    Posts & Comments

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,45 @@
+import React, { Fragment, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import PostItem from '../posts/PostItem';
+import CommentItem from '../post/CommentItem';
+import CommentForm from '../post/CommentForm';
+import { getPost, getPosts } from '../../actions/post';
+
+const Post = ({ getPost, post: { post, loading }, auth, match }) => {
+  useEffect(() => {
+    getPost(match.params.id);
+  }, [getPost, auth.loading]);
+
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to={'/posts'} className='btn'>
+        Back To Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className='comments'>
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
+    </Fragment>
+  );
+};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  post: state.post,
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { getPost })(Post);
